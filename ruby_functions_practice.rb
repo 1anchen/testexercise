9b77396d7c53cd56ba6e64847a5420e1c22b472a
pac9 <pac9@btinternet.com>
2018-04-10 16:00:24 +0100

    number to full name of month

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -30,3 +30,15 @@ end
 def add_string_as_number(string_1, string_2)
   return string_1.to_i + string_2.to_i
 end
+
+def number_to_full_month_name(month)
+ if month == 1
+   return "January"
+ elsif month == 3
+   return "March"
+ elsif month == 9
+   return "September"
+ else
+   return "Month not recognised"
+ end
+end
